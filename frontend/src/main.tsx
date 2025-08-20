f283688297951041a990aa68401cf93b28891629
Talib Mirza <mirzatalib110@gmail.com>
2025-08-20 14:45:37 -0500

    Adding analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { inject } from '@vercel/analytics'
+
+// Only inject in production
+if (import.meta.env?.MODE === 'production' || process.env.NODE_ENV === 'production') {
+	inject()
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
